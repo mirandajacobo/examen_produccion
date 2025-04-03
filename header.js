188c9515c9cfe1c2d3f5df4abd03a3d2dfbdd1e4
mirandajacobo <159475340+mirandajacobo@users.noreply.github.com>
2025-04-03 13:19:44 -0600

    Add files via upload

---
+++ header.js
@@ -0,0 +1,10 @@
+function cargarmenu{
+    let header = document.createElement('div')
+
+    let contx = document.createElement('div')
+
+    let tex = document.createElement('div')
+    tex.className = "tit-x";
+    tex.textContent = "total X ="; 
+    
+}
